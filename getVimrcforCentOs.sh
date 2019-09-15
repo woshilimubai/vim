47c7fd3823a56abe96211279def73d167238f469
Job <1769393445@qq.com>
2019-09-15 22:46:26 +0800

    Add files via upload

---
+++ getVimrcforCentOs.sh
@@ -0,0 +1,6 @@
+cd ~
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+sudo yum install ctags
+git clone https://github.com/woshilimubai/vim.git
+cp ~/vim/.vimrc ~/.vimrc
+rm -rf ~/vim
